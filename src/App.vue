0e14f4eb5229022455946bce11b0cca533decba1
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-04-29 20:51:31 -0300

    fix: clean initial projetc

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">
+          
+        </RouterLink>
       </nav>
    
 
