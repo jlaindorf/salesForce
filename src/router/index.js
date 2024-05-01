35f148541329456303e966aeef56f5641b874b04
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-04-30 22:26:42 -0300

    fix : rename type of router index

---
+++ src/router/index.js
@@ -0,0 +1,56 @@
+import {createRouter, createWebHistory} from 'vue-router'
+
+import HomeView from '@/views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
+import UserView from '@/views/users/UserView.vue'
+import ProductView from '@/views/products/ProductView.vue'
+import PaymentView from '@/views/payments/PaymentView.vue'
+import ClientView from '@/views/clients/ClientView.vue'
+import SaleView from '@/views/sales/SaleView.vue'
+
+    const router = createRouter({
+        history: createWebHistory(import.meta.env.BASE_URL),
+        routes:[
+         {
+
+            path:'/Home',
+            name: 'Home',
+            component: HomeView      
+        },
+        {
+            path:'/',
+            name: 'Login',
+            component: LoginView
+        },
+        
+        {
+            path:'/users',
+            name: 'users',
+            component: UserView
+        },
+        {
+            path:'/products',
+            name: 'products',
+            component: ProductView
+        },
+        {
+            path:'/payments',
+            name: 'payments',
+            component: PaymentView
+        },
+        {
+            path:'/clients',
+            name: 'clients',
+            component: ClientView
+        },
+        {
+            path:'/sales',
+            name: 'sales',
+            component: SaleView
+        }
+    ]
+
+   
+})
+
+export default router
