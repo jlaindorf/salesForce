448dea3228719f7c0291be78ec2ce8c73118ea11
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-05-01 17:37:52 -0300

    feat: add routes to create and update client

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import UserView from '@/views/users/UserView.vue'
 import ProductView from '@/views/products/ProductView.vue'
 import PaymentView from '@/views/payments/PaymentView.vue'
 import ClientView from '@/views/clients/ClientView.vue'
+import CreateClientView from '@/views/clients/CreateClientView.vue'
+import UpdateClientView from '../views/clients/UpdateClientView.vue'
 import SaleView from '@/views/sales/SaleView.vue'
 
     const router = createRouter({
@@ -42,6 +44,16 @@ import SaleView from '@/views/sales/SaleView.vue'
             path:'/clients',
             name: 'clients',
             component: ClientView
+        },
+        {
+            path:'/newclient',
+            name: 'newclient',
+            component: CreateClientView
+        },
+        {
+            path:'/updateclient/:id',
+            name: 'updateclients',
+            component: UpdateClientView
         },
         {
             path:'/sales',
